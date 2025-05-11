299a380164a99388f8b00bc2520d7a36783bc53b
Juraj Uhlar <juro.uhlar@gmail.com>
2025-05-11 18:47:50 +0100

    Add interactive.js for enhanced user interaction and update package.json with new dependencies and module type

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 #!/usr/bin/env node
 
+// You can run this script from anywhere with Node installed like this: 
+// npx --yes jurouhlar/jurouhlar
+// Stolen with ❤️ from https://egghead.io/lessons/npm-execute-code-from-a-remote-github-branch-with-npx   
+
 const fs = require("fs");
 const path = require("path");
 const output = fs.readFileSync(path.join(__dirname, "README.md"), "utf8");
--- interactive.js
+++ interactive.js
@@ -1,3 +1,51 @@
 #!/usr/bin/env node
 
-console.log("Something interactive");
+// You can run this script from anywhere like with node installed like this:
+// npx --yes -p jurouhlar/jurouhlar interactive
+// Stolen with ❤️ from https://egghead.io/lessons/npm-execute-code-from-a-remote-github-branch-with-npx
+
+import inquirer from 'inquirer';
+import cfonts from 'cfonts';
+import opn from 'opn';
+
+const actions = {
+  GitHub() {
+    opn("https://github.com/JuroUhlar");
+  },
+  Twitter() {
+    opn("https://x.com/JurajUhlar");
+  },
+  Blog() {
+    opn("https://medium.com/@jurouhlar");
+  },
+  LinkedIn() {
+    opn("https://www.linkedin.com/in/juraj-uhlar/");
+  },
+  Quit() {
+    process.exit(1);
+  },
+};
+
+cfonts.say("Juraj Uhlar", {
+  font: "chrome",
+  colors: ["blue", "white", "blue"],
+});
+
+console.log(`Greetings, my name is Juraj Uhlar.
+I am a Developer Experience Engineer.
+I work at Fingerprint.
+`);
+
+inquirer
+  .prompt([
+    {
+      type: "list",
+      name: "choice",
+      message: "Where would you like to go?",
+      choices: ["GitHub", "Twitter", "Blog", "LinkedIn", "Quit"],
+    },
+  ])
+  .then((answers) => {
+    actions[answers.choice]();
+    process.exit(1);
+  });
